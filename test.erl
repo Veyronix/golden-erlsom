0bc261426045a3c913fee1bcff8ea8ebd5f41df8
PS001 <luigi.zielone@gmail.com>
2018-05-28 22:22:49 +0200

    test commit, code rearrangement

--- test.erl
+++ test.erl
@@ -1,25 +1,34 @@
 %%%-------------------------------------------------------------------
 %%% @author Ludwik Ciechański
-%%% @copyright (C) 2018, <COMPANY>
-%%% @doc
-%%%
-%%% @end
+%%% @author Bartek Pietrzyk
 %%% Created : 20. maj 2018 13:45
 %%%-------------------------------------------------------------------
 -module(test).
 -author("Ludwik Ciechański").
+-author("Bartek Pietrzyk").
 
 %% API
 -export([]).
 -include("test_lib.hrl").
 
 %% user interface
--export([run/0,run2/0,n_last_rates_of_currency/2,when_currency_was_the_most_valuable_in_30_days/1]).
+-export([gbp_last_10/0, gold_last_30/0]).
+-export([n_last_rates_of_currency/2]).
+-export([when_currency_was_the_most_valuable_in_30_days/1]).
 
-n_last_rates_of_currency(N,Currency) ->
+when_most_valuable([],{Max_date, Max_value}) -> {Max_date, Max_value};
+when_most_valuable(Values, {Max_date, Max_value}) ->
+    [#currency_value{date = Data, value = Cena} | T] = Values,
+    if
+        Cena > Max_value -> when_most_valuable(T, {Data, Cena});
+        true             -> when_most_valuable(T, {Max_date, Max_value})
+    end.
+
+n_last_rates_of_currency(N, Currency) ->
     inets:start(),
     {ok, {{Version, 200, ReasonPhrase}, Headers, Body}} =
-      httpc:request("http://api.nbp.pl/api/exchangerates/rates/a/"++ Currency ++ "/last/"++integer_to_list(N)++"/?format=xml"),
+        httpc:request("http://api.nbp.pl/api/exchangerates/rates/a/"
+        ++ Currency ++ "/last/" ++ integer_to_list(N) ++ "/?format=xml"),
 
     {ok, Model} = erlsom:compile_xsd_file("ExchangeRatesSeries.xsd"),
     {ok, Result, _} = erlsom:scan(Body, Model),
@@ -32,7 +41,8 @@ n_last_rates_of_currency(N,Currency) ->
 when_currency_was_the_most_valuable_in_30_days(Currency) ->
     inets:start(),
     {ok, {{Version, 200, ReasonPhrase}, Headers, Body}} =
-      httpc:request("http://api.nbp.pl/api/exchangerates/rates/a/"++ Currency ++ "/last/30/?format=xml"),
+        httpc:request("http://api.nbp.pl/api/exchangerates/rates/a/"
+        ++ Currency ++ "/last/30/?format=xml"),
 
     {ok, Model} = erlsom:compile_xsd_file("ExchangeRatesSeries.xsd"),
     {ok, Result, _} = erlsom:scan(Body, Model),
@@ -41,13 +51,13 @@ when_currency_was_the_most_valuable_in_30_days(Currency) ->
     Currency_code = Result#'ExchangeRatesSeries'.'currency_code',
     Currency_measurements = Tmp#'ExchangeRatesSeries/Rates'.'currency_value',
     % Date = lists:foreach(fun print_currency_info/1, Currency_measurements).
-    [#currency_value{date = Data, value = Cena}|_] = Currency_measurements,
-    Date = when_most_valuable(Currency_measurements,{Data,Cena}). 
+    [#currency_value{date = Data, value = Cena} | _] = Currency_measurements,
+    Date = when_most_valuable(Currency_measurements, {Data, Cena}).
 
-run2() ->
+gbp_last_10() ->
     inets:start(),
     {ok, {{Version, 200, ReasonPhrase}, Headers, Body}} =
-      httpc:request("http://api.nbp.pl/api/exchangerates/rates/a/gbp/last/10/?format=xml"),
+        httpc:request("http://api.nbp.pl/api/exchangerates/rates/a/gbp/last/10/?format=xml"),
 
     {ok, Model} = erlsom:compile_xsd_file("ExchangeRatesSeries.xsd"),
     {ok, Result, _} = erlsom:scan(Body, Model),
@@ -56,49 +66,31 @@ run2() ->
     Currency_code = Result#'ExchangeRatesSeries'.'currency_code',
     Currency_measurements = Tmp#'ExchangeRatesSeries/Rates'.'currency_value',
     lists:foreach(fun print_currency_info/1, Currency_measurements).
-    
-when_most_valuable([],{Max_date,Max_value}) -> 
-    {Max_date,Max_value};
-when_most_valuable(Values,{Max_date,Max_value}) ->
-  [#currency_value{date = Data, value = Cena}|T] = Values,
-  if
-    Cena > Max_value ->
-        when_most_valuable(T,{Data,Cena});
-    true ->
-        when_most_valuable(T,{Max_date,Max_value})
-  end.
 
-run() ->
+gold_last_30() ->
     %% create HTTP client
     inets:start(),
       
-      %% request XML data from server
+    %% request XML data from server
     {ok, {{Version, 200, ReasonPhrase}, Headers, Body}} =
-      httpc:request("http://api.nbp.pl/api/cenyzlota/last/30/?format=xml"),
-      
-    %%file:write_file("src/golden_erlsom/dane.xml", Body).
+        httpc:request("http://api.nbp.pl/api/cenyzlota/last/30/?format=xml"),
     
-    %% compile xsd
+    %% compile XSD
     {ok, Model} = erlsom:compile_xsd_file("ArrayOfCenaZlota.xsd"),
     
     %% parse XML
-    %% {ok, #gold_store{gold=Golds}, _} = erlsom:scan(Body, Model).
     {ok, Result, _} = erlsom:scan(Body, Model),
-
     GoldStores = Result#'ArrayOfCenaZlota'.'gold_store',
       
-    %% test
+    %% print info for each record
     lists:foreach(fun print_gold_info/1, GoldStores).
 
 
 print_currency_info(#currency_value{date = Data, value = Cena}) ->
-    io:format("Date    : ~s~n
-                Value   : ~s~n~n", [formatDate(Data), Cena]).
-
+    io:format("Date    : ~s~nValue   : ~s~n~n", [formatDate(Data), Cena]).
 
 print_gold_info(#gold_type{date = Data, price = Cena}) ->
-  io:format("Date    : ~s~n
-Price   : ~s~n~n", [formatDate(Data), Cena]).
+    io:format("Date    : ~s~nPrice   : ~s~n~n", [formatDate(Data), Cena]).
 
 formatDate(undefined) -> "<unknown>";
 formatDate(Date) -> Date.
--- test_lib.hrl
+++ test_lib.hrl
@@ -5,9 +5,10 @@
 %% It is possible to add default values, but be aware that these will
 %% only be used when *writing* an xml document.
 
--record('gold_type', {anyAttribs, 'date', 'price'}).
--record('gold_store', {anyAttribs, 'gold_type'}).
--record('currency_value',{anyAttribs,'something','date','value'}).
--record('ExchangeRatesSeries/Rates',{anyAttribs,'currency_value'}).
 -record('ArrayOfCenaZlota',{anyAttribs,'gold_store'}).
+-record('gold_store', {anyAttribs, 'gold_type'}).
+-record('gold_type', {anyAttribs, 'date', 'price'}).
+
 -record('ExchangeRatesSeries',{anyAttribs,'table','currency_name','currency_code','ExchangeRatesSeries/Rates'}).
+-record('ExchangeRatesSeries/Rates',{anyAttribs,'currency_value'}).
+-record('currency_value',{anyAttribs,'something','date','value'}).
